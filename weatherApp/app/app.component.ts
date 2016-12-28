7295751a5689b92ac29191f36918f756cf674732
ujrc <uwjearc@yahoo.com>
2016-12-28 12:19:21 -0600

    Weather App using Open Street map

---
+++ weatherApp/app/app.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+
+@Component({
+selector:'app-root',
+template:'<h1>Weathe App</h1>'
+})
+
+export class AppComponent implements OnInit{
+
+    constructor(){}
+    
+    ngOnInit(){
+
+    }
+}
